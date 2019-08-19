05c4e8f28e61a783d1a13e44c1212e1b31810a05
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-08-19 14:50:30 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,53 @@
 #include "BinaryTree.h"
 #include <bits/stdc++.h>
 #include <string>
+#include "Cabecera.h"
+#include "Lateral.h"
+
 
 using namespace std;
 
-NodeABB * Aux2;
+NodeABB * AuxTree;
+Cabecera * LHeaders;
+Lateral * LLaterales;
+
 int contador =0;
 
 int main()
 {
   cout<<"insertar \n";
-    Aux2->SendInsert("Edgar");
-    Aux2->SendInsert("A");
-    Aux2->SendInsert("Edgar5");
-    Aux2->Graph();
-    Aux2->PrintNode(contador);
+    AuxTree->SendInsert("Edgar");
+    AuxTree->SendInsert("A");
+    AuxTree->SendInsert("Edgar5");
+    cout<<"InOrder \n";
+    AuxTree->Graph();
+    //Aux2->GraphPreOrder();
+    cout<<"PreOrder \n";
+    AuxTree->PrintPreOrder2();
+    //AuxTree->GraphPreOrder();
+    cout<<"PostOrder\n";
+    AuxTree->PrintPostOrder();
+    //Aux2->GraphPostOrder();
+
+    cout<<"Cabeceras \n";
+    LHeaders->InsertHeader(5);
+    LHeaders->InsertHeader(10);
+    LHeaders->InsertHeader(3);
+    LHeaders->InsertHeader(1);
+    LHeaders->InsertHeader(15);
+    cout<<"Imprimiendo Cabeceras \n";
+    LHeaders->PrintHeader();
+
+    cout<<"Laterales \n";
+    LLaterales->AddLateral(5);
+    LLaterales->AddLateral(10);
+    LLaterales->AddLateral(3);
+    LLaterales->AddLateral(8);
+    cout<<"Imprimiendo Laterales \n";
+    LLaterales->PrintLaterales();
+
+
+
+
    return 0;
 }
