d47a858322d5dc9a2fafa701d125045510ec80f3
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-08-20 21:36:26 -0600

    Update Lateral.h

--- Structures/Lateral.h
+++ Structures/Lateral.h
@@ -1,24 +1,38 @@
-#include <string>
+//
+// Created by edgarh on 20/08/19.
+//
 
-using namespace std;
+#ifndef P1CLASES_LATERAL_H
+#define P1CLASES_LATERAL_H
+
+#include "HorizontalList.h"
 
 struct NodeLateral{
     int Lateral;
     NodeLateral *NextLateral;
     NodeLateral *PreviousLateral;
+    HorizontalList Fila;
+
 };
 
-class Lateral{
+class Lateral {
 public:
-  Lateral();
-  NodeLateral *Create_Lateral(int Lateral);
-  void AddLateral(int Lateral);
-  void AddLateralStart(NodeLateral *& New_Lateral);
-  void AddLateralBetween(NodeLateral *& New_Lateral);
-  void AddLateralInEnd(NodeLateral *& New_Lateral);
-  void CoverLaterales();
-  bool IsLateral (int Lateral);
-  NodeLateral *SearchLateral(int Lateral);
-  void PrintLaterales();
-  string DotLaterales();
+    NodeLateral *FirstLateral=NULL;
+    NodeLateral *EndLateral=NULL;
+    NodeLateral * Create_Lateral(int Lateral);
+    void AddLateral(int NodeLateral);
+    void AddLateral2(NodeLateral *& New_Lateral);
+    void AddLateralStart(NodeLateral *& New_Lateral);
+    void AddLateralBetween(NodeLateral *& New_Lateral);
+    void AddLateralInEnd(NodeLateral *& New_Lateral);
+    void CoverLaterales();
+    bool IsLateral (int Lateral);
+    NodeLateral *SearchLateral(int Lateral);
+    void PrintLaterales();
+    string DotLaterales();
+
+    bool IsEmpty();
 };
+
+
+#endif //P1CLASES_LATERAL_H
