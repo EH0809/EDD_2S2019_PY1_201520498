56c0d2eccc552a0df582c50bb97e4c34cb35587f
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-08-20 21:36:12 -0600

    Update Cabecera.h

--- Structures/Cabecera.h
+++ Structures/Cabecera.h
@@ -1,26 +1,39 @@
-#include <string>
-#include <iostream>
-using namespace std;
+//
+// Created by edgarh on 20/08/19.
+//
 
+#ifndef P1CLASES_CABECERA_H
+#define P1CLASES_CABECERA_H
+#include <iostream>
+#include "string"
+#include "VerticalList.h"
+
+using namespace std;
 
 struct NodeHeader{
     int Header;
     NodeHeader *NextHeader;
     NodeHeader *PreviousHeader;
+    VerticalList Colum ;
 
 };
-
-class Cabecera{
+class Cabecera {
 public:
-  NodeHeader * New_Header(int Header);
-  void InsertHeader(int Header);
-  void InsertHeaderStart(NodeHeader *Header);
-  void InsertHeaderBetween(NodeHeader *Header);
-  void InsertHeaderInEnd(NodeHeader *Header);
-  void CoverHeader();
-  NodeHeader * SearchHeader(int Header);
-  bool IsHeader(int Header);
-  void PrintHeader();
-  string DotHeader();
-
+    NodeHeader *FirstHeader= NULL;
+    NodeHeader *EndHeader= NULL;
+    bool IsEmpty();
+    NodeHeader * New_Header(int Header);
+    void InsertHeader(int Header);
+    void InsertHeader2(NodeHeader *Header);
+    void InsertHeaderStart(NodeHeader *Header);
+    void InsertHeaderBetween(NodeHeader *Header);
+    void InsertHeaderInEnd(NodeHeader *Header);
+    void CoverHeader();
+    NodeHeader * SearchHeader(int Header);
+    bool IsHeader(int Header);
+    void PrintHeader();
+    string DotHeader();
 };
+
+
+#endif //P1CLASES_CABECERA_H
