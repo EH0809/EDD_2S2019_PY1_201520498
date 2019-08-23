820f5be2d3bb06d5c304d45d34c74a621659644e
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-08-23 12:15:52 -0600

    Update BinaryTree.h

--- Structures/BinaryTree.h
+++ Structures/BinaryTree.h
@@ -1,51 +1,60 @@
+//
+// Created by edgarh on 22/08/19.
+//
+
+#ifndef P1_BINARYTREE_H
+#define P1_BINARYTREE_H
 #include <string>
 #include <iostream>
-
 using namespace std;
 
 struct NodeBB{
-  std::string Name;
-  int Id;
-  NodeBB *SonLeft;
-  NodeBB *SonRight;
+    std::string Name;
+    int Id;
+    NodeBB *SonLeft;
+    NodeBB *SonRight;
 };
 
-class NodeABB
-{
-    public:
-        NodeABB();
-        int IdName(std::string Name);
-        NodeBB* Create_Node(string Name, int Id);
-        void InsertNode(NodeBB *&Tree,string Name);
-        void SendInsert(string Name);
-        void PrintNode(int Aux);
-        void PrintNode2(NodeBB *&Tree, int Aux);
+class BinaryTree {
+public:
+    int IdName(string Name);
+    NodeBB* Create_Node(string Name,int Id);
+    void SendInsert(string Name);
+    void InsertNode(NodeBB *&Tree,string Name);
+    void InsertNode2(NodeBB *&Tree,string Name);
 
-        string InOrder(NodeBB *& Tree,string Graph);
-        string PreOrder(NodeBB *& Tree,string Graph);
-        string PostOrder(NodeBB *& Tree,string Graph);
+    int Compare(string Name1 , string NameTree);
+    void PrintNode(int Aux);
+    void PrintNode2(NodeBB *&Tree, int Aux);
 
-        void Graph();
-        void GraphPreOrder();
-        void GraphPostOrder();
+    string InOrder(NodeBB * &Tree,string Graph);
+    string PreOrder(NodeBB * &Tree,string Graph);
+    string PostOrder(NodeBB * &Tree,string Graph);
 
-        void PrintPreOrder2();
-        void PrintPreOrder(NodeBB *& Tree);
-        void PrintPostOrder();
-        void PrintPostOrder2(NodeBB *& Tree);
+    void Graph();
+    void GraphPreOrder();
+    void GraphPostOrder();
+
+    void PrintPreOrder2();
+    void PrintPreOrder(NodeBB *&Tree);
+    void PrintPostOrder();
+    void PrintPostOrder2(NodeBB *&Tree);
 
 
-        string InOrderE(NodeBB *&Tree,string Graph);
-        void GraphE();
+    string InOrderE(NodeBB *&Tree,string Graph);
+    void GraphE();
 
-        NodeBB * Moretoleft(NodeBB *& Tree);
-        NodeBB * DeleteNode(NodeBB *& Tree, string Name);
-        void SentDelete(string Name);
+    NodeBB * Moretoleft(NodeBB *&Tree);
+    NodeBB * DeleteNode(NodeBB *&Tree, string Name);
+    void SentDelete(string Name);
 
-        NodeBB* ModifyNode(NodeBB *&Tree,string Name, string Nombre2);
-        void SentModify(string Name, string Nombre2);
-        NodeBB *SentFor(string Name);
-        NodeBB *SentFor2(NodeBB *&Tree, string Name);
+    NodeBB* ModifyNode(NodeBB *&Tree,string Name, string Nombre2);
+    void SentModify(string Name, string Nombre2);
+    NodeBB *SentFor(string Name);
+    NodeBB *SentFor2(NodeBB *&Tree, string Name);
 
-        string SentGraph();
+    string SentGraph();
 };
+
+
+#endif //P1_BINARYTREE_H
