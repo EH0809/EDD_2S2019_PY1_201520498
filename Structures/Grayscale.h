efa6675a91edd4c32b61abcfbf2d75e06af8b16f
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-09-10 02:14:03 -0600

    Create Grayscale.h

---
+++ Structures/Grayscale.h
@@ -0,0 +1,43 @@
+//
+// Created by edgarh on 9/09/19.
+//
+
+#ifndef P1_GRAYSCALE_H
+#define P1_GRAYSCALE_H
+
+
+#include "MatrixB.h"
+struct  GrayNode{
+    int Id;
+    int Col;
+    int Fil;
+    int ColorR;
+    int ColorG;
+    int ColorB;
+    string ColorHexa;
+    GrayNode *NextGray;
+    GrayNode *PreviuosGray;
+};
+class Grayscale {
+public:
+    GrayNode *FirstNode=NULL;
+    GrayNode *EndNode = NULL;
+    bool IsEmptyP();
+    GrayNode *Create_Node(int Pixel, int Col, int Fil, int ColorR, int ColorG,int ColorB, string ColorHexa);
+    void AddGRAYSCALE(int Pixel, int Col, int Fil, int ColorR, int ColorG,int ColorB, string ColorHexa);
+    void AddGRAYSCALEStart(GrayNode *& Node);
+    void AdDGRAYSCALEBetween(GrayNode *& Node);
+    void AddGRAYSCALEInEnd(GrayNode *& Node);
+    string  ConvertirAHexa(int ColorR, int ColorG,int ColorB, bool Con);
+    void Recibir(int WidthC, int HeightC,int WidthP, int HeightP);
+    void Tamanio(int Tama);
+    void CreateHTML();
+    void CreateCSS(int WidthC, int HeightC,int WidthP, int HeightP);
+    string Html(string html);
+    string Css(string css,int WidthC, int HeightC,int WidthP, int HeightP);
+};
+
+
+
+
+#endif //P1_GRAYSCALE_H
