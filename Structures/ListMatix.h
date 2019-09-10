88ad4880901dbaf205925bc620acbd04d3fa93eb
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-09-10 02:14:48 -0600

    Update ListMatix.h

--- Structures/ListMatix.h
+++ Structures/ListMatix.h
@@ -6,21 +6,24 @@
 #define P1_LISTMATIX_H
 #include "iostream"
 #include "Matrix.h"
-
+#include "CompleteMatrix.h"
+#include "MatrixB.h"
+#include "LinearizeMatrix.h"
 
 using namespace std;
 struct MatrixList{
     int Id;
     string Name;
     string Archivo;
-    Matrix PunteroMatrix;
+    LinearizeMatrix Linear;
+    MatrixB PunteroMatrixB;
     MatrixList *NextMatrix;
     MatrixList *PreviousMatrix;
 };
 class ListMatix {
 public:
-    MatrixList *FirstMatrixList;
-    MatrixList *EndMatrixList;
+    MatrixList *FirstMatrixList=NULL;
+    MatrixList *EndMatrixList= NULL;
     MatrixList *Create_Matrix(int Id, string Name);
     bool IsEmptyP();
     void AddMatrixList(int Id, string Name);
@@ -38,6 +41,26 @@ public:
     void GraphCapas(int Id);
     void MostarCapas();
     void ConvertRGBaHex();
+    void MandarCompleta(int id);
+    void GraficarMatizCompleta();
+    void AgregarMatrixCompleta();
+
+    void GraficarLFilas();
+    void MandarHtml();
+    void MandarCSS(int WidthC, int HeightC, int WidthP, int HeightP);
+    void MandarTamanio(int WidthM, int HeightM);
+
+    //*****************Mandar GRAYSCALE ******//////////
+    void MandarHtmGRAYSCALEl();
+    void MandarCSSGRAYSCALE(int WidthC, int HeightC, int WidthP, int HeightP);
+    //******************MANDAR NEGATIVE *********////////
+    void MandarHtmlNEGATIVE();
+    void MandarCSSNEGATIVE(int WidthC, int HeightC, int WidthP, int HeightP);
+
+    //***********************MANDAR POR CAPAS *****///////////
+    void GrayscapeCapas(int Id);
+    void NegativeCapas(int Id);
+
 
 };
 
