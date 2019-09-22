221cd7448cb19d9b25e060df4bf0d46fb1bad733
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-09-21 20:34:39 -0600

    Create LinealizarMirrorY.h

---
+++ Structures/LinealizarMirrorY.h
@@ -0,0 +1,14 @@
+//
+// Created by edgarh on 17/09/19.
+//
+
+#ifndef P1_LINEALIZARMIRRORY_H
+#define P1_LINEALIZARMIRRORY_H
+
+
+class LinealizarMirrorY {
+
+};
+
+
+#endif //P1_LINEALIZARMIRRORY_H
