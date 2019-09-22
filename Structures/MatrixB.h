9e5e513ce9b7ae41e02bd158402f079a533103e8
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-09-21 20:35:58 -0600

    Update MatrixB.h

--- Structures/MatrixB.h
+++ Structures/MatrixB.h
@@ -7,18 +7,6 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-/*
-struct NodeMatrixB{
-    int Col;
-    int Fil;
-    int ColorR;
-    int ColorG;
-    int ColorB;
-    NodeMatrixB *RightNodeMatrix;
-    NodeMatrixB *LeftNodeMatrix;
-    NodeMatrixB *UpNodeMatrix;
-    NodeMatrixB *DownNodeMatrix;
-};*/
 class NodeMatrixB2{
 public:
     int Col;
@@ -31,7 +19,7 @@ public:
     NodeMatrixB2 *UpNodeMatrix;
     NodeMatrixB2 *DownNodeMatrix;
 
-    NodeMatrixB2(int Col, int Fil, int ColoR, int ColorG, int ColorB){
+    NodeMatrixB2(int Col, int Fil, int ColoR, int ColorG, int ColorB) {
         this->Col = Col;
         this->Fil = Fil;
         this->ColorR = ColoR;
@@ -42,30 +30,18 @@ public:
         this->UpNodeMatrix = NULL;
         this->DownNodeMatrix = NULL;
     }
+    int print(){
+
+    }
 };
 
 class MatrixB {
 public:
-//    NodeMatrixB *FirstNode;
     NodeMatrixB2 *HeadNode;
     MatrixB (){
-        //NodeMatrixB *Aux = new NodeMatrixB;
         NodeMatrixB2 *Aux = new NodeMatrixB2(0,0,0,0,0);
         HeadNode = Aux;
     }
-
-    /*
-    NodeMatrixB *CreateNode(int Col, int Fil, int ColorR, int ColorG , int ColorB);
-    void AddMatrix(int Col, int Fil, int ColorR, int ColorG , int ColorB);
-    void AddMatrixCabecera(int Col, int Fil, int ColorR, int ColorG , int ColorB );
-    void AddMatrixHeader(int Col, int Fil, int ColorR, int ColorG , int ColorB );
-    void CreateCabecera(int Col);
-    void CreateHeaders(int Fil);
-    void GraficarMatrix();
-    string DotMatrix(string dot);
-    string DotMatirx2(string Dot);
-*/
-
     //**************************CLASE ***************************//
     void AddMatrixC(int Col, int Fil, int ColorR, int ColorG , int ColorB);
     void AddMatrixCabeceraC(int Col, int Fil, int ColorR, int ColorG , int ColorB );
@@ -77,8 +53,11 @@ public:
     string DotMatirx2C(string Dot);
     string DotCompleta();
 
+    void Graficar();
 
     string DotGrayScape(string Dot);
     string DotNegative(string Dot);
+
+    void EditNodeMatrix(int Col, int Fil , int ColorR, int ColorG, int ColorB);
 };
 #endif //P1_MATRIXB_H
